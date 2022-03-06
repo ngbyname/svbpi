50fcb3cf4a2574f8b57e356b135faeb40a4264ab
Ashwani Ashwani <ashwani.ashwani@africa.airtel.com>
2022-03-06 23:15:04 +0530

    2nd commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'svbpiApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('svbpiApp');
+    expect(app.title).toEqual('SVBPI - A Place to Learn a Chance to Grow');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'svbpiApp';
+  title = 'SVBPI - A Place to Learn a Chance to Grow';
+  constructor(private titleService:Title) {
+  }
+ 
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
