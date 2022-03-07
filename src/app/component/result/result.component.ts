a850d1304088e3a1ffeb0f73c511d670c31a0ebc
raoashwani <raoashwani786@gmail.com>
2022-03-07 22:46:08 +0530

    enroll form

--- src/app/component/result/result.component.ts
+++ src/app/component/result/result.component.ts
@@ -10,34 +10,43 @@ export class ResultComponent implements OnInit {
   submitted = false;
   // registrationForm:FormGroup
   // City names
-  City: any = ['Florida', 'South Dakota', 'Tennessee', 'Michigan']
+  // City: any = ['Florida', 'South Dakota', 'Tennessee', 'Michigan']
   constructor(
     public fb: FormBuilder,
     private cd: ChangeDetectorRef
   ) { }
   /*##################### Registration Form #####################*/
   registrationForm = this.fb.group({
-    file: [null],
+    // file: [null],
     fullName: this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[_A-z0-9]*((-|\s)*[_A-z0-9])*$')]],
       lastName: ['', [Validators.required]]
     }),
-    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-    phoneNumber: ['', [Validators.required, Validators.maxLength(10), Validators.pattern('^[0-9]+$')]],
-    address: this.fb.group({
-      street: ['', [Validators.required]],
-      city: ['', [Validators.required]],
-      cityName: ['', [Validators.required]]
+    parentsDetails: this.fb.group({
+      fatherName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[_A-z0-9]*((-|\s)*[_A-z0-9])*$')]],
+      motherName: ['', [Validators.required]]
     }),
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    dob: ['', [Validators.required]],
+    courseName: ['', [Validators.required]],
+    enrollmentNo: ['', [Validators.required]],
+    centerCode: ['', [Validators.required]],
+    session: ['', [Validators.required]],
+    phoneNumber: ['', [Validators.required, Validators.maxLength(10), Validators.pattern('^[0-9]+$')]],
+    // address: this.fb.group({
+    //   street: ['', [Validators.required]],
+    //   city: ['', [Validators.required]],
+    //   cityName: ['', [Validators.required]]
+    // }),
     gender: ['male'],
-    PasswordValidation: this.fb.group({
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    },{
-      validator: ValidatePassword.MatchPassword // your validation method
-    }
-    ),
-    addDynamicElement: this.fb.array([])
+    // PasswordValidation: this.fb.group({
+    //   password: ['', Validators.required],
+    //   confirmPassword: ['', Validators.required]
+    // },{
+    //   validator: ValidatePassword.MatchPassword // your validation method
+    // }
+    // ),
+    // addDynamicElement: this.fb.array([])
   })  
 
   /*########################## File Upload ########################*/
@@ -78,29 +87,30 @@ export class ResultComponent implements OnInit {
   }  
  
   // Getter method to access formcontrols
-  get myForm() {
-    return this.registrationForm.controls;
-  }
+  // get myForm() {
+  //   return this.registrationForm.controls;
+  // }
   
   // Choose city using select dropdown
-  changeCity(e) {
-    this.registrationForm.get('address.cityName').setValue(e.target.value, {
-      onlySelf: true
-    })
-  }
+  // changeCity(e) {
+  //   this.registrationForm.get('address.cityName').setValue(e.target.value, {
+  //     onlySelf: true
+  //   })
+  // }
 
   /*############### Add Dynamic Elements ###############*/
-  get addDynamicElement() {
-    return this.registrationForm.get('addDynamicElement') as FormArray
-  }
+  // get addDynamicElement() {
+  //   return this.registrationForm.get('addDynamicElement') as FormArray
+  // }
 
-  addSuperPowers() {
-    this.addDynamicElement.push(this.fb.control(''))
-  }
+  // addSuperPowers() {
+  //   this.addDynamicElement.push(this.fb.control(''))
+  // }
 
   // Submit Registration Form
   onSubmit() {
     this.submitted = true;
+    console.log(this.registrationForm.value)
     if(!this.registrationForm.valid) {
       alert('Please fill all the required fields to create a super hero!')
       return false;
