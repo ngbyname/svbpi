a56359c86d5287e79a4d8dd1e6ef6f64dbf6e189
ankitkhantwal <ankitkchetu@gmail.com>
2022-04-05 17:46:36 +0530

    merge code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { SliderComponent } from './slider/slider.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import {MaterialModule} from '../../material.module';
+import { MaterialModule } from '../../material.module';
+import { BannerComponent } from './banner/banner.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
-  declarations: [NavigationComponent, SliderComponent, FooterComponent, HeaderComponent],
+  declarations: [NavigationComponent, SliderComponent, FooterComponent, HeaderComponent, BannerComponent],
   imports: [
     CommonModule,
     MaterialModule,
-
+    RouterModule
   ],
-  exports:[HeaderComponent,NavigationComponent,FooterComponent]
+  exports:[HeaderComponent, NavigationComponent, FooterComponent, BannerComponent]
 })
 export class SharedModule { }
