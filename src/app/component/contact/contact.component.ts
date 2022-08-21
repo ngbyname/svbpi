f4325e94acb403275c5ae3832fa4544aa8c3b39d
Manish Kumar Singh <manish1.singh@africa.airtel.com>
2022-08-22 02:27:20 +0530

    fix contact page

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+  public pageTitleText:string="Contact Us"
   constructor() { }
 
   ngOnInit(): void {
