750c9e8cbd29ce26ac34a1fd45d39946216565cc
Manish Kumar Singh <manish1.singh@africa.airtel.com>
2022-09-03 19:56:39 +0530

    course details

--- src/app/component/courses/courses.component.ts
+++ src/app/component/courses/courses.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-
+  public pageTitleText:'Course Details';
   constructor() { }
 
   ngOnInit(): void {
--- src/app/component/svbpi-page.module.ts
+++ src/app/component/svbpi-page.module.ts
@@ -6,15 +6,17 @@ import { MatCarouselModule } from '@ngmodule/material-carousel';
 import { ReactiveFormsModule } from '@angular/forms';
 import{RegisterComponent} from './register/register.component';
 import { AdmitcardComponent } from './admitcard/admitcard.component';
-import { CoursesComponent } from './courses/courses.component';
+// import { CoursesComponent } from './courses/courses.component';
 import {NgxPrintModule} from 'ngx-print';
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
-  declarations: [ComponentComponent,HomeComponent,RegisterComponent,AdmitcardComponent,CoursesComponent],
+  declarations: [ComponentComponent,HomeComponent,RegisterComponent],
   imports: [
     CommonModule,
     MatCarouselModule.forRoot(),
     ReactiveFormsModule,
-    NgxPrintModule
+    NgxPrintModule,
+    SharedModule
   ],
   exports:[
 
